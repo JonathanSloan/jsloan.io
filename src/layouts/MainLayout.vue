e086d6b9119aae93fdcf18123eed63465cb3e21c
Sloan <jonathanssloan@protonmail.com>
2021-07-06 19:17:53 -0400

    Rebuild 3

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,7 +10,7 @@
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
           </q-avatar> -->
           
-          Rebuild 1
+          Rebuild 3
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
